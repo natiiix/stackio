253097e6b96a6b2c696441841c8120089e34512c
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-02 19:57:54 +0200

    Move the LF to <br> conversion where it belongs

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,10 @@
 const { ipcRenderer } = require('electron');
 
-ipcRenderer.on('print-output', (event: Electron.Event, str: string) => document.getElementById('output').innerHTML = str);
+ipcRenderer.on('print-output', (event: Electron.Event, str: string) =>
+    document.getElementById('output').innerHTML = str.replace(/\n/g, '<br>')
+);
 
 document.getElementById('run').addEventListener('click', (e) => {
     const code = (document.getElementById('code') as HTMLTextAreaElement).value;
-    ipcRenderer.send('run-code', code.replace(/\n/g, '<br>'));
+    ipcRenderer.send('run-code', code);
 });
