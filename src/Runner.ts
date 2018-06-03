d6bc7d0e3e54de9860fe2d592a3be7b3a028f4d1
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-03 15:18:11 +0200

    Implement comments, ignore empty lines and clean up the code

--- src/Runner.ts
+++ src/Runner.ts
@@ -11,30 +11,38 @@ export class Runner {
         const state = new State();
 
         this.operations.forEach((x, i) => {
+            // Empty line or a comment
+            if (x.length === 0 || x[0] === '#') {
+                return;
+            }
+
+            // Try to convert the instruction into a number
+            const num = Number(x);
+            // If the instruction is a valid numeric value
+            if (!isNaN(num)) {
+                // Push the number onto the stack
+                state.push(num);
+                return;
+            }
+
+            // If the instruction is a string literal
+            if (x.length > 2 && x[0] === '"' && x[x.length - 1] === '"') {
+                // Print the value of string string
+                // Replace "\n" with actual new line characters
+                printCallback(x.substr(1, x.length - 2).replace(/\\n/g, '\n'));
+                return;
+            }
+
             switch (x.toLowerCase()) {
+                // PRINT
                 case 'print':
                     printCallback(state.get().toString());
                     break;
 
+                // Unrecognized instruction
                 default:
-                    const num = Number(x);
-
-                    if (!isNaN(num)) {
-                        // If the instruction is a valid numeric value
-                        // Push the number onto the stack
-                        state.push(num);
-                    } else if (x.length > 2 && x[0] === '"' && x[x.length - 1] === '"') {
-                        // If the instruction is a string literal
-                        // Print the value of string string
-                        // Replace "\n" with actual new line characters
-                        printCallback(x.substr(1, x.length - 2).replace(/\\n/g, '\n'));
-                    } else {
-                        // Unrecognized instruction
-                        // Throw an error
-                        throw Error(`Invalid instruction on line ${i + 1}: ${x}`);
-                    }
-
-                    break;
+                    // Throw an error
+                    throw Error(`Invalid instruction on line ${i + 1}: ${x}`);
             }
         });
     }
