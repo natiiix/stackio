ddffb31f7ac32b450bdd0ba701f34e766a5703e8
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-03 14:52:57 +0200

    Add simple code execution login to Runner Implement number pushing instruction Implement string literal printing instrunction Implement PRINT instrunction

--- src/Runner.ts
+++ src/Runner.ts
@@ -1,11 +1,41 @@
+import { State } from './State';
+
 export class Runner {
     private readonly operations: string[];
 
-    constructor(code: string) {
+    public constructor(code: string) {
         this.operations = code.split('\n');
     }
 
-    run(printCallback: (message: string) => void): void {
-        this.operations.forEach((x, i) => printCallback(`${i + 1}: ${x}\n`));
+    public run(printCallback: (message: string) => void): void {
+        const state = new State();
+
+        this.operations.forEach((x, i) => {
+            switch (x.toLowerCase()) {
+                case 'print':
+                    printCallback(state.get().toString());
+                    break;
+
+                default:
+                    const num = Number(x);
+
+                    if (!isNaN(num)) {
+                        // If the instruction is a valid numeric value
+                        // Push the number onto the stack
+                        state.push(num);
+                    } else if (x.length > 2 && x[0] === '"' && x[x.length - 1] === '"') {
+                        // If the instruction is a string literal
+                        // Print the value of string string
+                        // Replace "\n" with actual new line characters
+                        printCallback(x.substr(1, x.length - 2).replace(/\\n/g, '\n'));
+                    } else {
+                        // Unrecognized instruction
+                        // Throw an error
+                        throw Error(`Invalid instruction on line ${i + 1}: ${x}`);
+                    }
+
+                    break;
+            }
+        });
     }
 }
