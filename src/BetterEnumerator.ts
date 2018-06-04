ed055c67be8ff87116ea5198f8ed528c7db2ff02
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-04 11:21:51 +0200

    Modify BetterEnumerator to make index a publicly accessible property

--- src/BetterEnumerator.ts
+++ src/BetterEnumerator.ts
@@ -1,6 +1,6 @@
 export class BetterEnumerator<T> {
     private array: T[];
-    private index: number;
+    private internalIndex: number;
 
     public constructor(arr: T[]) {
         if (arr.length === 0) {
@@ -8,46 +8,58 @@ export class BetterEnumerator<T> {
         }
 
         this.array = arr;
-        this.index = 0;
+        this.internalIndex = 0;
     }
 
     public movePrevious(): boolean {
-        const canMove = this.index > 0;
+        const canMove = this.internalIndex > 0;
 
         if (canMove) {
-            this.index--;
+            this.internalIndex--;
         }
 
         return canMove;
     }
 
     public moveNext(): boolean {
-        const canMove = this.index + 1 < this.array.length;
+        const canMove = this.internalIndex + 1 < this.array.length;
 
         if (canMove) {
-            this.index++;
+            this.internalIndex++;
         }
 
         return canMove;
     }
 
     public moveFirst(): void {
-        this.index = 0;
+        this.internalIndex = 0;
     }
 
     public moveLast(): void {
-        this.index = this.array.length - 1;
+        this.internalIndex = this.array.length - 1;
     }
 
     public get current(): T {
-        return this.array[this.index];
+        return this.array[this.internalIndex];
     }
 
     public get previous(): T {
-        return this.array[this.index - 1];
+        return this.array[this.internalIndex - 1];
     }
 
     public get next(): T {
-        return this.array[this.index + 1];
+        return this.array[this.internalIndex + 1];
+    }
+
+    public set index(index: number) {
+        if (index < 0 || index >= this.array.length) {
+            throw Error('Invalid index: ' + index.toString());
+        }
+
+        this.internalIndex = index;
+    }
+
+    public get index(): number {
+        return this.internalIndex;
     }
 }
